74c5d62464df9e7b0104d0996e118dcad853d5a5
Matthew Brady <w3matt@gmail.com>
2020-08-02 17:35:28 +0100

    v1.0 - new project detection model

--- import_yocto_bm.py
+++ import_yocto_bm.py
@@ -7,10 +7,11 @@ import sys
 import platform
 import re
 import subprocess, shutil
-import tempfile, time
+import time
 from blackduck.HubRestApi import HubInstance
 
 u = uuid.uuid1()
+print("Yocto build manifest import into Black Duck Utility v1.0\n")
 
 parser = argparse.ArgumentParser(description='Import Yocto build manifest to BD project version', prog='import_yocto_bm')
 
@@ -21,7 +22,7 @@ parser.add_argument("-v", "--version", help="Black Duck project version to creat
 parser.add_argument("-y", "--yocto_build_folder", help="Yocto build folder (required if CVE check required or manifest file not specified)", default=".")
 parser.add_argument("-o", "--output_json", help="Output JSON bom file for manual import to Black Duck (instead of uploading the scan automatically)", default="")
 parser.add_argument("-t", "--target", help="Yocto target (default core-poky-sato)", default="core-image-sato")
-parser.add_argument("-m", "--manifest", help="Input build manifest file (if not specified will be determined from conf files)", default="")
+parser.add_argument("-m", "--manifest", help="Input build license.manifest file (if not specified will be determined from conf files)", default="")
 parser.add_argument("--arch", help="Architecture (if not specified then will be determined from conf files)", default="")
 parser.add_argument("--cve_check_only", help="Only check for patched CVEs from cve_check and update existing project", action='store_true')
 parser.add_argument("--no_cve_check", help="Skip check for and update of patched CVEs", action='store_true')
@@ -40,6 +41,8 @@ def check_args():
 	if not os.path.isdir(args.yocto_build_folder):
 		print("Specified Yocto build folder '{}' does not exist\nExiting".format(args.yocto_build_folder))
 		return(False)
+	else:
+		args.yocto_build_folder = os.path.abspath(args.yocto_build_folder)
 
 	if args.cve_check_file != "" and args.no_cve_check:
 		print("Options cve_check_file and no_cve_check cannot be specified together".format(args.cve_check_file))
@@ -65,11 +68,11 @@ def check_args():
 
 def check_env():
 	if platform.system() != "Linux":
-		print("Please use this program on a Linux platform where Yocto project has been installed\nExiting")
+		print("Please use this program on a Linux platform where Yocto project has been built\nExiting")
 		return(False)
 
 	# Check oe-pkgdata-util and bitbake commands are on PATH
-	if shutil.which("bitbake") is None or shutil.which("oe-pkgdata-util") is None:
+	if shutil.which("bitbake") is None or shutil.which("bitbake-layers") is None:
 		print("Please ensure Yocto project has been installed and environment has been set (run 'source ooe-init-build-env')\nExiting")
 		return(False)
 	return(True)
@@ -86,17 +89,18 @@ def check_yocto_build_folder():
 
 	for d in yocto_build_folders:
 		if not os.path.isdir(os.path.join(args.yocto_build_folder, d)):
-			print("Project folder {} does not appear to be a Yocto project folder which has been built({} folder missing)\nExiting".format(args.yocto_build_folder, d))
+			print("Project build folder {} does not appear to be a Yocto project folder which has been built ({} folder missing)\nExiting".format(args.yocto_build_folder, d))
 			return(False)
 
 	for f in yocto_files:
 		if not os.path.isfile(os.path.join(args.yocto_build_folder, f)):
-	 		print("Project folder {} does not appear to be a Yocto project folder ({} file missing)\nExiting".format(args.yocto_build_folder, f))
+	 		print("Project build folder {} does not appear to be a Yocto project folder ({} file missing)\nExiting".format(args.yocto_build_folder, f))
 	 		return(False)
 	return(True)
 
+licdir = ""
 def find_files():
-	global args
+	global args, licdir
 
 	# Locate yocto files & folders
 	bbconf = os.path.join(args.yocto_build_folder, "..", "meta", "conf", "bitbake.conf")
@@ -115,23 +119,31 @@ def find_files():
 	deploydir = ""
 	machine = ""
 
-	c = open(bbconf, "r")
-	for line in c:
-		if re.search('^TMPDIR ', line):
-			tmpdir = line.split()[2]
-		if re.search('^DEPLOY_DIR ', line):
-			deploydir = line.split()[2]
-	c.close()
+	try:
+		c = open(bbconf, "r")
+		for line in c:
+			if re.search('^TMPDIR ', line):
+				tmpdir = line.split()[2]
+			if re.search('^DEPLOY_DIR ', line):
+				deploydir = line.split()[2]
+		c.close()
+	except Exception as e:
+		print("ERROR: Unable to read bitbake.conf file {}\n".format(bbconf) + str(e))
+		return(False)
 
-	l = open(locconf, "r")
-	for line in l:
-		if re.search('^TMPDIR ', line):
-			tmpdir = line.split()[2]
-		if re.search('^DEPLOY_DIR ', line):
-			deploydir = line.split()[2]
-		if re.search('^MACHINE ', line):
-			machine = line.split()[2]
-	l.close()
+	try:
+		l = open(locconf, "r")
+		for line in l:
+			if re.search('^TMPDIR ', line):
+				tmpdir = line.split()[2]
+			if re.search('^DEPLOY_DIR ', line):
+				deploydir = line.split()[2]
+			if re.search('^MACHINE ', line):
+				machine = line.split()[2]
+		l.close()
+	except Exception as e:
+		print("ERROR: Unable to read local.conf file {}\n".format(locconf) + str(e))
+		return(False)
 
 	if tmpdir != "":
 		tmpdir = tmpdir.replace('${TOPDIR}', args.yocto_build_folder)
@@ -148,14 +160,17 @@ def find_files():
 	if args.arch == "":
 		args.arch = machine.strip('"')
 
+	licdir = os.path.join(deploydir, "licenses")
 	if args.manifest == "":
-		licdir = os.path.join(deploydir, "licenses")
 		manifestdir = ""
+		if not os.path.isdir(licdir):
+			print("License directory {} does not exist - has Yocto project been built?".format(licdir))
+			return(False)
 		for file in sorted(os.listdir(licdir)):
 			if file.startswith(args.target + "-" + args.arch + "-"):
 				manifestdir = os.path.join(licdir, file)
 
-		manifestfile = os.path.join(manifestdir, "package.manifest")
+		manifestfile = os.path.join(manifestdir, "license.manifest")
 		if not os.path.isfile(manifestfile):
 			print("Build manifest file {} does not exist - either build Yocto project or use -m option to specify build manifest file\nExiting".format(manifestfile))
 			return(False)
@@ -201,6 +216,8 @@ def upload_json(jsonfile):
 bdio = []
 proj = args.project
 ver = args.version
+comps_layers = []
+comps_recipes = []
 if not args.cve_check_only:
 	try:
 		i = open(args.manifest, "r")
@@ -209,146 +226,147 @@ if not args.cve_check_only:
 		sys.exit(3)
 
 	try:
-		if args.output_json != "":
-			o = open(args.output_json, "w")
-		else:
-			o = tempfile.NamedTemporaryFile(delete=False, suffix=".jsonld")
-			args.output_json = o.name
+		liclines = i.readlines()
+		i.close()
 	except Exception as e:
-		print('ERROR: Unable to open output json file {} \n'.format(args.output_json) + str(e))
+		print('ERROR: Unable to read license.manifest file {} \n'.format(args.manifest) + str(e))
 		sys.exit(3)
 
-	pkglines = i.readlines()
-	i.close()
+	print("\nProcessing recipes from license.manifest: ...")
+	recipes = {}
+	entries = 0
+	for line in liclines:
+		arr = line.split(":")
+		if len(arr) > 1:
+			key = arr[0]
+			value = arr[1].strip()
+			if key == "PACKAGE VERSION":
+				ver = value
+			elif key == "RECIPE NAME":
+				entries += 1
+				if not value in recipes.keys():
+					recipes[value] = ver
+	print("Identified {} recipes from {} packages".format(len(recipes), entries))
 
-	print("\nProcessing packages: ...")
-
-	kernel = ""
-	recipes = []
-	count = 0
-	for line in pkglines:
-		linesplit = line.split()
-		#if count > 30:
-		#	break
-		if len(linesplit) == 0:
-			break
-		try:
-			count += 1
-			pkg = linesplit[0]
-			print(pkg)
-			if pkg.startswith("kernel-") and not "-module-" in pkg:
-				x = re.search("^kernel\-[\d\.]*\-", pkg)
-				if x != None:
-					arr = pkg.split("-")
-					kernel = arr[0] + "/" + arr[1]
+	print("Identifying layers for recipes ...")
+	output = subprocess.check_output(['bitbake-layers', 'show-recipes', '*'], stderr=subprocess.STDOUT)
+	mystr = output.decode("utf-8").strip()
+	lines = mystr.splitlines()
+	recipe_layer = {}
+	layers = []
+	rec = ""
+	start = False
+	for line in lines:
+		if start:
+			if rec != "":
+				arr = line.split()
+				if len(arr) > 1:
+					layer = arr[0]
+					ver = arr[1]
+					recipe_layer[rec] = layer
+					if layer not in layers:
+						layers.append(layer)
+				rec = ""
 			else:
-				output = subprocess.check_output(['oe-pkgdata-util', 'package-info', linesplit[0]], stderr=subprocess.STDOUT)
-				mystr = output.decode("utf-8").strip()
-				re.sub(".*RPROVIDES.*\n?","",mystr)
-				recipes.append(mystr)
-		except:
-			print("ERROR: Unexpected response from oe-pkgdata-util command - please check Yocto environment before continuing - exiting")
-			sys.exit(1)
+				arr = line.split(":")
+				rec = arr[0]
+		elif line.find("=== Matching recipes: ===") != -1:
+			start = True
+	print("Discovered {} layers".format(len(layers)))
 
-	print("Extracted recipes for {} packages in build manifest\n".format(count))
+	print("Identifying recipe revisions: ...")
+	for recipe in recipes.keys():
+		recipeinfo = os.path.join(licdir, recipe, "recipeinfo")
+		if os.path.isfile(recipeinfo):
+			try:
+				r = open(recipeinfo, "r")
+				reclines = r.readlines()
+				r.close()
+			except Exception as e:
+				print("ERROR: unable to open recipeinfo file {}\n".format(recipeinfo) + str(e))
+				sys.exit(3)
+			for line in reclines:
+				if line.find("PR:") != -1:
+					arr = line.split(":")
+					rev = arr[1].strip()
+					recipes[recipe] += "-" + rev
 
-	pkgtree = {}
-	allpkgs = []
-	toppkgs = []
-
-	print("Processing recipes: ...")
-	lines = 0
-	for line in recipes:
-		linesplit = line.split()
-		if len(linesplit) == 0:
-			break
-		pkg = linesplit[0] + "/" + linesplit[1]
-		parent = linesplit[2] + "/" + linesplit[3]
-		allpkgs.append(pkg)
-
-		if pkg == parent:
-			toppkgs.append(pkg)
-		elif parent not in pkgtree:
-			pkgtree[parent] = [ pkg ]
-		else:
-			pkgtree[parent].append(pkg)
-		lines += 1
-
-	print("Processed {} recipes".format(lines, args.manifest))
-
-	#print(json.dumps(pkgtree, indent=4))
+	print("Processing layers: ...")
+	#proj_rel is for the project relationship (project to layers)
 	proj_rel = []
-	components = []
-
-	if kernel != "":
+	comps_layers = []
+	for layer in layers:
 		proj_rel.append(
-		{
-			"related": "http:debian//" + pkg,
-			"relationshipType": "DYNAMIC_LINK"
-		}
+			{
+				"related": "http:yocto/" + layer + "/1.0",
+				"relationshipType": "DYNAMIC_LINK"
+			}
 		)
-
-	print("\nCreating bdio json file ...")
-	for pkg in toppkgs:
-		pkgname, pkgver = pkg.split("/")
-		proj_rel.append(
-		{
-			"related": "http:yocto/meta/" + pkg,
-			"relationshipType": "DYNAMIC_LINK"
-		}
-		)
-
-	for pkg in pkgtree:
-		if pkg not in toppkgs:
-			pkgname, pkgver = pkg.split("/")
-			proj_rel.append(
-		{
-			"related": "http:yocto/meta/" + pkg,
-			"relationshipType": "DYNAMIC_LINK"
-		}
-			)
-		if pkg not in allpkgs:
-			allpkgs.append(pkg)
-
-	for pkg in allpkgs:
-		rel = []
-		if pkg in pkgtree:
-			for child in pkgtree[pkg]:
-				#print("    " + child)
-				rel.append(
-		{
-			"related": "http:yocto/meta/" + child,
-			"relationshipType": "DYNAMIC_LINK"
-		}
+		layer_rel = []
+		for recipe in recipes.keys():
+			if recipe_layer[recipe] == layer:
+				layer_rel.append(
+					{
+						"related": "http:yocto/" + layer + "/" + recipe + "/" + recipes[recipe],
+						"relationshipType": "DYNAMIC_LINK"
+					}
 				)
 
-		pkgname, pkgver = pkg.split("/")
-		components.append(
-	  {
-		"@id": "http:yocto/meta/" + pkg,
-		"@type": "Component",
-		"externalIdentifier": {
-		"externalSystemTypeId": "@yocto",
-		"externalId": "meta/" + pkg,
-		"externalIdMetaData": {
-		"forge": {
-			"name": "yocto",
-			"separator": "/",
-			"usePreferredNamespaceAlias": True
-		},
-		"pieces": [
-			pkgname,
-			pkgver
-		],
-		"prefix": "meta"
-	      }
-	    },
-	    "relationship": rel
-	  })
+		comps_layers.append(
+		{
+			"@id": "http:yocto/" + layer + "/1.0",
+			"@type": "Component",
+			"externalIdentifier": {
+			"externalSystemTypeId": "@yocto",
+			"externalId": layer,
+			"externalIdMetaData": {
+			"forge": {
+				"name": "yocto",
+				"separator": "/",
+				"usePreferredNamespaceAlias": True
+			},
+			"pieces": [
+				layer,
+				"1.0"
+			],
+			"prefix": "meta"
+		      }
+		    },
+		    "relationship": layer_rel
+		})
+
+	print("Processing recipes: ...")
+	comps_recipes = []
+	for recipe in recipes.keys():
+		comps_recipes.append(
+		{
+			"@id": "http:yocto/" + recipe_layer[recipe] + "/" + recipe + "/" + recipes[recipe],
+			"@type": "Component",
+			"externalIdentifier": {
+			"externalSystemTypeId": "@yocto",
+			"externalId": recipe_layer[recipe] + "/" + recipe + "/" + recipes[recipe],
+			"externalIdMetaData": {
+			"forge": {
+				"name": "yocto",
+				"separator": "/",
+				"usePreferredNamespaceAlias": True
+			},
+			"pieces": [
+				recipe,
+				recipes[recipe]
+			],
+			"prefix": recipe_layer[recipe]
+		      }
+		    },
+		    "relationship": []
+		  })
+
+	#proj_rel is for the project relationship (project to layers)
+
 	mytime = datetime.datetime.now()
 	bdio_header = {
 	    "specVersion": "1.1.0",
-	    "spdx:name": proj + "/" + ver + " yocto/bom",
+	    "spdx:name": args.project + "/" + args.version + " yocto/bom",
 	    "creationInfo": {
 	      "spdx:creator": [
 		"Tool: Detect-6.3.0",
@@ -362,13 +380,13 @@ if not args.cve_check_only:
 	  }
 
 	bdio_project = {
-	    "name": proj,
-	    "revision": ver,
-	    "@id": "http:yocto/" + proj,
+	    "name": args.project,
+	    "revision": args.version,
+	    "@id": "http:yocto/" + args.project + "/" + args.version,
 	    "@type": "Project",
 	    "externalIdentifier": {
 	      "externalSystemTypeId": "@yocto",
-	      "externalId": "yocto/" + proj + "/" + ver,
+	      "externalId": "yocto/" + args.project + "/" + args.version,
 	      "externalIdMetaData": {
 		"forge": {
 		  "name": "yocto",
@@ -376,8 +394,8 @@ if not args.cve_check_only:
 		  "usePreferredNamespaceAlias": True
 		},
 		"pieces": [
-		  proj,
-		  ver
+		  args.project,
+		  args.version
 		],
 		"prefix": ""
 	      }
@@ -387,16 +405,34 @@ if not args.cve_check_only:
 
 	#print("Writing json output file {} ...".format(args.output_json))
 
-	bdio = [ bdio_header, bdio_project, components ]
-	o.write(json.dumps(bdio, indent=4).encode())
-	o.close()
+	bdio = [ bdio_header, bdio_project, comps_layers, comps_recipes ]
+	if args.output_json != "":
+		try:
+			o = open(args.output_json, "w")
+			o.write(json.dumps(bdio, indent=4))
+			o.close()
+			print("Json project file written to {} - must be manually uploaded".format(args.output_json))
+		except Exception as e:
+			print("ERROR: Unable to write output json file {}\n".format(args.output_json) + str(e))
+		sys.exit(1)
+	else:
+		import tempfile
+		try:
+			with tempfile.NamedTemporaryFile(suffix=".jsonld", delete=False) as o:
+				args.output_json = o.name
+				o.write(json.dumps(bdio, indent=4).encode())
+				o.close()
+		except Exception as e:
+			print("ERROR: Unable to write temporary output json file\n" + str(e))
+			sys.exit(3)
 
-	print("Uploading scan ...")
+	print("\nUploading scan to Black Duck server ...")
 	if upload_json(args.output_json):
 		print("Scan file uploaded successfully\n")
 	else:
 		print("ERROR: Unable to upload scan file")
 		sys.exit(3)
+
 
 def process_patched_cves(vuln_list):
 	hub = HubInstance()
@@ -411,8 +447,8 @@ def process_patched_cves(vuln_list):
 		response = hub.execute_get(vulnerable_components_url, custom_headers=custom_headers)
 		vulnerable_bom_components = response.json().get('items', [])
 
-	except:
-		print("ERROR: Unable to extract vulnerabilities from project via API")
+	except Exception as e:
+		print("ERROR: Unable to extract vulnerabilities from project via API\n" + str(e))
 		return()
 
 	status = "PATCHED"
@@ -433,14 +469,20 @@ def process_patched_cves(vuln_list):
 				#else:
 				#	print("Skipped {} (Component not in image)".format(vuln_name))
 
-		print("{} CVEs marked as patched in project {} version {}".format(count, args.project, args.version))
-	except:
-		print("ERROR: Unable to update vulnerabilities via API")
+		print("{} CVEs marked as patched in project '{}/{}'".format(count, args.project, args.version))
+	except Exception as e:
+		print("ERROR: Unable to update vulnerabilities via API\n" + str(e))
 
 def wait_for_bom_completion(ver):
+	# Check job status
 	uptodate = False
 	try:
 		links = ver['_meta']['links']
+		#link = next((item for item in links if item["rel"] == "codelocations"), None)
+		#cl_url = link['href']
+		#custom_headers = {'Accept':'application/vnd.blackducksoftware.internal-1+json'}
+		#resp = hub.execute_get(cl_url, custom_headers=custom_headers)
+
 		link = next((item for item in links if item["rel"] == "bom-status"), None)
 
 		href = link['href']
@@ -454,7 +496,8 @@ def wait_for_bom_completion(ver):
 			resp = hub.execute_get(href, custom_headers=custom_headers)
 			uptodate = resp.json()['upToDate']
 			loop += 1
-	except:
+	except Exception as e:
+		print("ERROR: {}".format(str(e)))
 		return(False)
 
 	if uptodate:
@@ -466,14 +509,14 @@ def wait_for_bom_completion(ver):
 if args.cve_check_file != "" and not args.no_cve_check:
 	hub = HubInstance()
 
-	print("\nWaiting for scan completion before continuing ...")
+	print("Waiting for scan completion before continuing ...")
 	if not args.cve_check_only:
 		time.sleep(30)
 
 	try:
 		ver = hub.get_project_version_by_name(args.project, args.version)
-	except:
-		print("ERROR: Unable to get project version from API")
+	except Exception as e:
+		print("ERROR: Unable to get project version from API\n" + str(e))
 		sys.exit(1)
 
 	if not wait_for_bom_completion(ver):
